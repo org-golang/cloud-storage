95d7c68b490e106bbfb956f2c932d657be37bab8
xgbnl <745502274@qq.com>
2024-07-16 14:27:26 +0800

    test

--- src/test/golang/auth_provider_test.go
+++ src/test/golang/auth_provider_test.go
@@ -1,6 +1,7 @@
 package golang
 
 import (
+	"fmt"
 	"github.com/org-golang/cloud-storage/src/main/golang"
 	"github.com/org-golang/cloud-storage/src/main/golang/configuration"
 	"strings"
@@ -11,15 +12,26 @@ func TestAuth(t *testing.T) {
 
 	properties := configuration.BuildProperties("./cloud.yaml")
 
-	alibaba := golang.NewAlibabaAuth(properties)
+	alibaba, err := golang.NewAuthProvider(properties, golang.ALIBABA)
+
+	if err.Error() {
+		t.Error(err.GetMessage())
+	}
 
 	if !strings.Contains(alibaba.GetAuthorizePage(), "openapi.alipan.com") {
 		t.Errorf("Not included: [%s]", "openapi.alipan.com")
 	}
 
-	baidu := golang.NewBaiduAuth(properties)
+	baidu, err := golang.NewAuthProvider(properties, golang.BAIDU)
+
+	if err.Error() {
+		t.Error(err.GetMessage())
+	}
 
 	if !strings.Contains(baidu.GetAuthorizePage(), "openapi.baidu.com") {
 		t.Errorf("Not included: [%s]", "openapi.baidu.com")
 	}
+
+	fmt.Println(alibaba.GetAuthorizePage())
+	fmt.Println(baidu.GetAuthorizePage())
 }
